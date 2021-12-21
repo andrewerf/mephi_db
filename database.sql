2163f85cae7b2a82d150f4c597af7e782736b28b
Andrew <andrew-aralov@yandex.ru>
2021-12-21 23:53:50 +0300

    Fixed serials

--- database.sql
+++ database.sql
@@ -35,7 +35,7 @@ create table image
     path               varchar,
     ql_path            varchar,
     other_paths        character varying[],
-    product_id         serial,
+    product_id         integer,
     foreign key (product_id) references product(id),
     foreign key (source_id) references source(id)
 );
@@ -43,8 +43,8 @@ create table image
 create table vector
 (
     id       serial primary key,
-    class_id serial not null,
-    image_id serial,
+    class_id integer not null,
+    image_id integer,
     other    json,
     geometry geometry,
     foreign key (class_id) references vector_class(id),
@@ -66,7 +66,7 @@ create table dataset
 
 create table image_planet
 (
-    id        serial,
+    id        integer,
     cloud_map geometry,
     foreign key (id) references image(id)
         on update cascade on delete cascade
@@ -74,7 +74,7 @@ create table image_planet
 
 create table image_dg
 (
-    id            serial,
+    id            integer,
     cloud_map     geometry,
     stereo_number integer,
     foreign key (id) references image(id)
@@ -83,7 +83,7 @@ create table image_dg
 
 create table image_ntsomz
 (
-    id       serial,
+    id       integer,
     hash     varchar not null,
     scene    integer not null,
     selected boolean not null,
@@ -105,10 +105,10 @@ create table vector_image_dataset
 create table experiment
 (
     id         serial primary key,
-    author_id  serial,
+    author_id  integer,
     datetime   timestamp,
     git_commit varchar not null,
-    dataset_id serial,
+    dataset_id integer,
     parent_id  integer,
     foreign key (parent_id) references experiment(id),
     foreign key (dataset_id) references dataset(id),
@@ -118,7 +118,7 @@ create table experiment
 create table model
 (
     id            serial primary key,
-    experiment_id serial,
+    experiment_id integer,
     type          varchar             not null,
     paths         character varying[] not null,
     other         json,
@@ -127,9 +127,9 @@ create table model
 
 create table vector_author
 (
-    vector_id serial,
-    author_id serial,
-    model_id  serial,
+    vector_id integer,
+    author_id integer,
+    model_id  integer,
     foreign key (vector_id) references vector(id),
     foreign key (author_id) references author(id),
     foreign key (model_id) references model(id)
